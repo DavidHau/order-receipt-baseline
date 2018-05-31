c7970f7cd16bd8023837dad8c2a9a1c56a915e77
David Hau <davidphiliphk@gmail.com>
2018-05-31 16:41:26 +0800

    extract append lineItems

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -1,11 +1,13 @@
 package org.katas.refactoring;
 
+import java.util.List;
+
 /**
  * OrderReceipt prints the details of order including customer name, address, description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- * 
+ *
  */
 public class OrderReceipt {
     private Order order;
@@ -20,11 +22,10 @@ public class OrderReceipt {
 		appendHeader(output);
 		appendCustomerDetails(output);
 
+        appendLineItems(output, order.getLineItems());
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
-			appendLineItem(output, lineItem);
-
 			// calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
             totSalesTx += salesTax;
@@ -46,15 +47,17 @@ public class OrderReceipt {
 		output.append("Sales Tax").append('\t').append(totSalesTx);
 	}
 
-	public void appendLineItem(StringBuilder output, LineItem lineItem) {
-		output.append(lineItem.getDescription());
-		output.append('\t');
-		output.append(lineItem.getPrice());
-		output.append('\t');
-		output.append(lineItem.getQuantity());
-		output.append('\t');
-		output.append(lineItem.totalAmount());
-		output.append('\n');
+	public void appendLineItems(StringBuilder output, List<LineItem> lineItems) {
+		for (LineItem lineItem : order.getLineItems()) {
+			output.append(lineItem.getDescription());
+			output.append('\t');
+			output.append(lineItem.getPrice());
+			output.append('\t');
+			output.append(lineItem.getQuantity());
+			output.append('\t');
+			output.append(lineItem.totalAmount());
+			output.append('\n');
+		}
 	}
 
 	private void appendCustomerDetails(StringBuilder output) {
